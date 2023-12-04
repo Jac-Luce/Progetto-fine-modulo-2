163a9438b3e6fb021b29954cc1c37d6b0d722a4f
Jac-Luce <luce.jac92@gmail.com>
2023-12-04 16:08:43 +0100

    first commit

---
+++ parte2.js
@@ -0,0 +1,135 @@
+const jobs = [
+    { title: "Marketing Intern", location: "US, NY, New York" },
+    {
+      title: "Customer Service - Cloud Video Production",
+      location: "NZ, Auckland",
+    },
+    {
+      title: "Commissioning Machinery Assistant (CMA)",
+      location: "US, IA, Wever",
+    },
+    {
+      title: "Account Executive - Washington DC",
+      location: "US, DC, Washington",
+    },
+    { title: "Bill Review Manager", location: "US, FL, Fort Worth" },
+    { title: "Accounting Clerk", location: "US, MD," },
+    { title: "Head of Content (m/f)", location: "DE, BE, Berlin" },
+    {
+      title: "Lead Guest Service Specialist",
+      location: "US, CA, San Francisco",
+    },
+    { title: "HP BSM SME", location: "US, FL, Pensacola" },
+    {
+      title: "Customer Service Associate - Part Time",
+      location: "US, AZ, Phoenix",
+    },
+    {
+      title: "ASP.net Developer Job opportunity at United States,New Jersey",
+      location: "US, NJ, Jersey City",
+    },
+    {
+      title: "Talent Sourcer (6 months fixed-term contract)",
+      location: "GB, LND, London",
+    },
+    {
+      title: "Applications Developer, Digital",
+      location: "US, CT, Stamford",
+    },
+    { title: "Installers", location: "US, FL, Orlando" },
+    { title: "Account Executive - Sydney", location: "AU, NSW, Sydney" },
+    {
+      title: "VP of Sales - Vault Dragon",
+      location: "SG, 01, Singapore",
+    },
+    { title: "Hands-On QA Leader", location: "IL, Tel Aviv, Israel" },
+    {
+      title: "Southend-on-Sea Traineeships Under NAS 16-18 Year Olds Only",
+      location: "GB, SOS, Southend-on-Sea",
+    },
+    { title: "Visual Designer", location: "US, NY, New York" },
+    {
+      title: "Process Controls Engineer - DCS PLC MS Office - PA",
+      location: "US, PA, USA Northeast",
+    },
+    { title: "Marketing Assistant", location: "US, TX, Austin" },
+    { title: "Front End Developer", location: "NZ, N, Auckland" },
+    { title: "Engagement Manager", location: "AE," },
+    {
+      title: "Vice President, Sales and Sponsorship (Businessfriend.com)",
+      location: "US, CA, Carlsbad",
+    },
+    { title: "Customer Service", location: "GB, LND, London" },
+    { title: "H1B SPONSOR FOR L1/L2/OPT", location: "US, NY, New York" },
+    { title: "Marketing Exec", location: "SG," },
+    {
+      title: "HAAD/DHA Licensed Doctors Opening in UAE",
+      location: "AE, AZ, Abudhabi",
+    },
+    {
+      title: "Talent Management Process Manager",
+      location: "US, MO, St. Louis",
+    },
+    { title: "Customer Service Associate", location: "CA, ON, Toronto" },
+    {
+      title: "Customer Service Technical Specialist",
+      location: "US, MA, Waltham",
+    },
+    { title: "Software Applications Specialist", location: "US, KS," },
+    { title: "Craftsman Associate", location: "US, WA, Everett" },
+    { title: "Completion Engineer", location: "US, CA, San Ramon" },
+    { title: "I Want To Work At Karmarama", location: "GB, LND," },
+    {
+      title: "English Teacher Abroad",
+      location: "US, NY, Saint Bonaventure",
+    },
+  ]
+
+  
+  let myButton = document.getElementById("searchButton");
+  myButton.addEventListener("click", (event) => { 
+    
+    let inputJob = document.getElementById("jobTitle").value;
+    let inputLocation = document.getElementById("location").value;
+    inputJob = jobs.title;
+    inputLocation = jobs.location;
+    let objectResult = [];
+    
+    for (let i = 0; i < jobs.length; i++){
+      let job = jobs[i];
+      let newInputJob = job.title.toLowerCase();
+      let newInputLocation = job.location.toLowerCase();
+      
+      if ((newInputJob.includes(inputJob)) && (newInputLocation.includes(inputLocation))) {
+        
+        let result = {
+            title: job.title,
+            location: job.location
+        };
+        objectResult.push(result);
+      }
+      
+    }
+    return {
+        result: objectResult,
+        count: objectResult.length
+    };
+   
+  });
+  
+  function table(objectResult) {
+    let body = document.querySelector("body");
+    for (let i = 0 ; i < objectResult.result.length; i++){
+      let element = objectResult[i];
+      let result = "<tr> <td>" + element.title + "</td> <td>" + element.location + "</td> </tr>";
+      body.innerHTML = body.innerHTML + result;
+    }
+  }
+  
+  //let body = document.querySelector("body");
+  //let ul = document.createElement("ul");
+  // body.appendChild(ul);
+  // let li = document.createElement("li");
+  // li.innerHTML = jobs.title + " - " + jobs.location;
+  // ul.append(li);
+   
